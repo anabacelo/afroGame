23d33e0196d815448d234f2dbee9545a9a6a5f38
Vitor Hugo <55327320+Vitorhgbds@users.noreply.github.com>
2020-09-29 17:35:05 -0300

    criando endless e script init

---
+++ Afro Game/Assets/Sprites/Protagonistas/Scripts/EndlessMov.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndlessMov : MonoBehaviour
+{
+    public float speed = 10f;
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponentInParent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)){
+
+        }
+        if(Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)){
+            
+        }
+
+    }
+
+    private void FixedUpdate() {
+        rb.velocity = Vector3.forward * speed;
+    }
+}
