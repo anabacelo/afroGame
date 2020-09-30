d9873e1367b50ebe82a65fefe6c0fdc3ca8363b7
Vitor Hugo <55327320+Vitorhgbds@users.noreply.github.com>
2020-09-30 14:17:28 -0300

    endlessRun Init

--- Afro Game/Assets/Sprites/Protagonistas/Scripts/EndlessMov.cs
+++ Afro Game/Assets/Sprites/Protagonistas/Scripts/EndlessMov.cs
@@ -5,26 +5,44 @@ using UnityEngine;
 public class EndlessMov : MonoBehaviour
 {
     public float speed = 10f;
+    public float lineSpeed = 10f;
     public Rigidbody rb;
+    private float currentLine = 0f;
+    Vector3 verticalTargetPosition;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponentInParent<Rigidbody>();
+        verticalTargetPosition  = new Vector3(transform.position.x, 2, transform.position.z);
     }
 
     // Update is called once per frame
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)){
-
+            changeLine(-2f);
         }
         if(Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)){
+            changeLine(2f);
             
         }
 
+        Vector3 targetPosition = new Vector3(verticalTargetPosition.x, verticalTargetPosition.y, transform.position.z);
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, lineSpeed * Time.deltaTime);
     }
 
     private void FixedUpdate() {
         rb.velocity = Vector3.forward * speed;
+    }
+
+
+    void changeLine(float direction){
+        float targetLine = currentLine + direction;
+        if(targetLine < -2f || targetLine > 2f){
+            return;
+        }
+        currentLine = targetLine;
+        verticalTargetPosition = new Vector3((currentLine), transform.position.y, 0);
     }
 }
