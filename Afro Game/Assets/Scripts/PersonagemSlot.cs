567fd01898aa35bcf849c363b8d413c95f6cc27f
willian.albeche <willian.albeche@edu.pucrs.br>
2020-12-30 15:28:45 -0300

    Seleção em andamento
    parte dos botões

--- Afro Game/Assets/Scripts/PersonagemSlot.cs
+++ Afro Game/Assets/Scripts/PersonagemSlot.cs
@@ -1,104 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
-using System;
 using UnityEngine;
-using UnityEngine.UI;
-using TMPro;
+using UnityEngine.SceneManagement;
+
 
 public class PersonagemSlot : MonoBehaviour
 {        
-        [SerializeField]
-        GameObject Selector;
-        [SerializeField]
-        GameObject[] col1;
-        [SerializeField]
-        GameObject[] col2;
-
-        const int collum = 2;
-
-        int positionRow;
-        List<int> charList = new List<int>();
-        public GameObject[] rows= new GameObject[collum];
-        GameObject currentChar;
-
-        void AddRowtoRowArray(int index, GameObject[] col){
-            for (int i = 0; i < 2; i++)
-            {
-                rows[index] = col[i];
-            }
-        }
-
-
-        void Start(){
-            AddRowtoRowArray(0,col1);
-            AddRowtoRowArray(1,col2);
-        }
-
-        void Update(){
+        public static int indexChar = -1;
+        public void SelectCharacter(int index){ 
+            indexChar = index;
+            SceneManager.LoadScene("Room");
 
         }
-
-
-
-
-
-
-
-
-
-
-
-
-
-        // private int selectedCharacterIndex;
-        // private Color desiredColor;
-
-        
-        // [Header("Lista de personagens")]
-        // [SerializeField] private List<CharacterSelectObject> charaterList = new List<CharacterSelectObject>();
-
-        // [Header("UI References")]
-        // [SerializeField] private TextMeshProUGUI characterName;
-        // [SerializeField] private Image characterSplash;
-        // [SerializeField] private Image backgroundColor;
-
-
-        // private void Start(){
-        //     UpdateCharacterSelectionUi();
-        // }
-
-        // public void LeftArrow(){
-        //     selectedCharacterIndex--;
-        //     if(selectedCharacterIndex < 0){
-        //         selectedCharacterIndex = charaterList.Count -1;
-        //     }
-        //     UpdateCharacterSelectionUi();
-        // }
-
-        // public void RightArrow(){
-        //     selectedCharacterIndex++;
-        //     if(selectedCharacterIndex ==charaterList.Count){
-        //         selectedCharacterIndex = 0;
-        //     }
-        //     UpdateCharacterSelectionUi();
-        // }
-
-        // private void UpdateCharacterSelectionUi(){
-        //     characterSplash.sprite = charaterList[selectedCharacterIndex].splash;
-        //     characterName.text = charaterList[selectedCharacterIndex].characterName;
-        // }
-
-        // [System.Serializable]
-        // public class CharacterSelectObject{
-        //     public Sprite splash;
-        //     public string characterName;
-
-        // }
-
-        // public int SelectedCharacter(){
-            
-        //     PlayerPrefs.SetInt(string.Format("Personagem{0}"), selectedCharacterIndex);
-
-        //     return selectedCharacterIndex;
-        // }
 }
